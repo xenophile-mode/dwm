2e472782c433c55703b5d56c6270d5cd640cec2a
xenophile-mode <erickgpersonalacct1@gmail.com>
2020-12-24 15:15:51 -0600

    switched terminal to xterm

--- config.h
+++ config.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-l", "25", NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "xterm", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
